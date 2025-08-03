e535499ed01f8a31b87bcd0ccb45ae5e5be8674f
nyneferreira <nyneferreira@gmail.com>
2025-08-03 13:37:53 -0300

    Initial commit

---
+++ src/test/java/com/janinechaves/cadastro_usuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janinechaves.cadastro_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
